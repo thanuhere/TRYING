e9df408dc9640488bb979ffd7ae2fcdb88aa2857
Nandhalal <nandhalalgnair@gmail.com>
2022-08-06 22:16:18 +0530

    commit

--- QUIZ/script.js
+++ QUIZ/script.js
@@ -7,12 +7,36 @@ const answerButtonElement = document.getElementById('answer-buttons')
 
 let shufledQuestions, currentQuestionIndex;
 let Quizscore=0;
+
+startButton.addEventListener('click',startButton)
+
+nextButton.addEventListener('click',()=>{
+    currentQuestionIndex++
+    setnextQuestion()
+})
+
+function startGame(){
+    startButton.classList.add('hide')
+    shufledQuestions=Questions.sort(() =>Math.random()-0.5)
+    currectQuestionIndex=0;
+    questionContainerElement.classList.remove('hide')
+    setnextQuestion()
+    Quizscore=0
+}
+
+function setnextQuestion(){
+    resetState();
+    showQuestion(shufledQuestions[currentQuestionIndex])
+}
+
 function showQuestion(question){
     questionrElement.innerText=question.question;
     question.answers.forEach((answer) =>{
         const button=document.createElement('button')
         button.innerText=answer.text;
-        button.classList.add('btn'){
+        button.classList.add('btn')
+        if (answer.correct)
+        {
             button.dataset.correct= answer.correct
         }
         button.addEventListener('click',selectAnswer)
@@ -39,9 +63,9 @@ function selectAnswer(e)
     const selectedButton=e.target
     const correct= selectedButton.dataset.correct
     setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button) => {
+    Array.from(answerButtonsElement.children).forEach((button) =>{
         setStatusClass(button, button.dataset.correct)
-    }
+     })
     if(shufledQuestions.length> currentQuestionIndex +1){
         nextButton.classList.remove("hide")
     }    
@@ -80,12 +104,27 @@ function clearStatusClass(element){
 
 const Questions=[
     { 
-        question: 'Which of the following is a javascript framework?';
-        answers=[
+        question: 'Which of the following is a javascript framework?',
+        answers:[
+            {text: 'Python', correct: false},
+            {text: 'django', correct: false},
+            {text: 'React', correct: true},
             {text: 'Python', correct: false}
-            {text: 'django', correct: false}
-            {text: 'React', correct: true}
-            {text: 'Python', correct: false}
-        ]
-    }
+        ],
+    },
+    { 
+        question: 'Which is the Prime Minister of India',
+        answers:[
+            {text: 'Narendra Modi', correct: true},
+            {text: 'Rahul Gandhi', correct: false}
+            
+        ],
+    },
+    { 
+        question: '4*3',
+        answers:[
+            {text: '6', correct: false},
+            {text: '12', correct: true},
+        ],
+    },
 ]
